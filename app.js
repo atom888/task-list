f528484fdfabca28353883c6ca013ef296b721c3
Adam Dipinto <adam.dipinto@gmail.com>
2018-12-21 07:09:33 -0800

    Remove and clear task event functions added. Remove individual tasks or clear full task section

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ loadEventListeners();
 function loadEventListeners() {
     //Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear task event
+    clearBtn.addEventListener('click', clearTasks);
 }
 
 // Add Task
@@ -41,4 +45,20 @@ function addTask(e) {
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+    if (e.target.parentElement.classList.contains('delete-item')) {
+        if (confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear Tasks
+function clearTasks() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
 }
